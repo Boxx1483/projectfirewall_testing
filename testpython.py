8c8c02fb38460b19281c6c21dc519569c3506ecc
Boxx1483 <31645679+Boxx1483@users.noreply.github.com>
2018-04-19 13:08:37 +0200

    Update testpython.py

--- testpython.py
+++ testpython.py
@@ -21,6 +21,11 @@ def main():
   elif ipclass is False:
     menu_outside()
 
+  else:
+      print('You are not connected correctly')
+      exit()
+
+
 
 def menu_inside():
     print('Inside Menu')
@@ -46,11 +51,47 @@ def menu_inside():
         print('kys')
         menu_inside()
 
+def menu_outside():
+    print('Outside Menu')
+    print('-' * 25)
+    print('1. Test FW')
+    print('2. Test Webserver')
+    print('3. Test Work Station')
+    print('4. To exit program')
+    print('-' * 25)
+    user_inside = str(input('Enter your choice:'))
+    if user_inside == '1':
+        testfw_out()
 
+    elif user_inside == '2':
+        testweb_out()
+
+    elif user_inside == '3':
+        testws_out()
+
+    elif user_inside == '4':
+        exit()
+    else:
+        print('kys')
+    menu_outside()
+
+def testfw_in():
+
+def testweb_in():
+    
+def testws_in():
+
+def testfw_out():
+
+def testweb_out():
+
+def testws_out():
 
 
 
 main()
+
+
 
 
 
