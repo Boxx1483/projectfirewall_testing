bec98f4d72f361d3984d43bf21f4f3a7f8e5c147
Boxx1483 <31645679+Boxx1483@users.noreply.github.com>
2018-04-19 12:59:28 +0200

    Update testpython.py

--- testpython.py
+++ testpython.py
@@ -1,6 +1,56 @@
 #This script will test our network
-import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.connect(("8.8.8.8", 80))
 print(s.getsockname()[0])
 s.close()
+#This script will test our network
+
+#imports
+import subprocess
+import socket
+import socketserver
+
+#Global_Constants
+ipclass = socket.
+
+
+def main():
+  if ipclass is True:
+    menu_inside()
+
+  elif ipclass is False:
+    menu_outside()
+
+
+def menu_inside():
+    print('Inside Menu')
+    print('-'*25)
+    print('1. Test FW')
+    print('2. Test Webserver')
+    print('3. Test Work Station')
+    print('4. To exit program')
+    print('-'*25)
+    user_inside = str(input('Enter your choice:'))
+    if user_inside == '1':
+        testfw_in()
+
+    elif user_inside == '2':
+        testweb_in()
+
+    elif user_inside == '3':
+        testws_in()
+
+    elif user_inside == '4':
+        exit()
+    else:
+        print('kys')
+        menu_inside()
+
+
+
+
+
+main()
+
+
+
